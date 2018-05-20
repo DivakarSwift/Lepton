4c12b124f56fc0681512c06922432e5e7fec9c15
xspyhack <xspyhack@gmail.com>
2018-05-20 14:11:29 +0800

    Initial project

---
+++ Lepton/Lepton.h
@@ -0,0 +1,19 @@
+//
+//  Lepton.h
+//  Lepton
+//
+//  Created by bl4ckra1sond3tre on 20/05/2018.
+//  Copyright © 2018 blessingsoftware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Lepton.
+FOUNDATION_EXPORT double LeptonVersionNumber;
+
+//! Project version string for Lepton.
+FOUNDATION_EXPORT const unsigned char LeptonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Lepton/PublicHeader.h>
+
+
