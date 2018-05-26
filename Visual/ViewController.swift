fbfd8ed47ccddd833f293cc9d5ebec6fa87bf45c
xspyhack <xspyhack@gmail.com>
2018-05-26 19:36:13 +0800

    Update

--- Visual/ViewController.swift
+++ Visual/ViewController.swift
@@ -16,8 +16,6 @@ class ViewController: UIViewController {
 
     var asset: AVURLAsset?
 
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
